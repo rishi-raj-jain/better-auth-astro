3d4cb90a7ac617a3eefbd932c5a0d49c87e2caca
Rishi Raj Jain <jain71000@gmail.com>
2024-11-23 22:00:05 +0000

    .

---
+++ src/auth.ts
@@ -0,0 +1,10 @@
+import { betterAuth } from "better-auth";
+import pkg from 'pg'
+const { Pool } = pkg;
+
+export const auth = betterAuth({
+    database: new Pool({ connectionString: process.env.DATABASE_URL }),
+    emailAndPassword: {
+        enabled: true
+    },
+})
